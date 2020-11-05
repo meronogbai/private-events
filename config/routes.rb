024bfeb08b3e32e1c0bb69ec55da3bab70a66054
Meron Ogbai <okbaymeron@gmail.com>
2020-11-05 17:59:51 +0300

    Add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources 'users', only: %i[new create show]
   get 'signup', to: 'users#new'
   get 'hello_world/index'
