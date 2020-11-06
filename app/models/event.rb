cd85185b62a3ef5beb249f8c34cfb6aeabf5e8f4
Meron Ogbai <okbaymeron@gmail.com>
2020-11-06 17:20:12 +0300

    Add upcoming and past methods in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,20 @@
 class Event < ApplicationRecord
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
+
+  def self.past
+    result = []
+    all.each do |event|
+      result << event if event.event_date < Date.today
+    end
+    result
+  end
+
+  def self.upcoming
+    result = []
+    all.each do |event|
+      result << event if event.event_date > Date.today
+    end
+    result
+  end
 end
