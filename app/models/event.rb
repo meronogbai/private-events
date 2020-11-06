e83d351c08ce76734d6a6d166d3c6cc68edb106a
Meron Ogbai <okbaymeron@gmail.com>
2020-11-06 18:17:05 +0300

    Refactor event queries

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,23 +2,6 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
 
-  # puts Date.today-100
-  #
-  # scope :past_scoped, -> { self.where(event_date: '2020-04-20') }
-
-  def self.past
-    result = []
-    all.each do |event|
-      result << event if event.event_date < Date.today
-    end
-    result
-  end
-
-  def self.upcoming
-    result = []
-    all.each do |event|
-      result << event if event.event_date > Date.today
-    end
-    result
-  end
+  scope :past, -> { where('event_date<?', Date.today) }
+  scope :upcoming, -> { where('event_date>?', Date.today) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,10 @@ class User < ApplicationRecord
   has_many :attended_events, through: :attendances, source: :event
 
   def upcoming_events
-    result = []
-    attended_events.each do |event|
-      result << event if event.event_date > Date.today
-    end
-    result
+    attended_events.where('event_date>?', Date.today)
   end
 
   def previous_events
-    result = []
-    attended_events.each do |event|
-      result << event if event.event_date < Date.today
-    end
-    result
+    attended_events.where('event_date<?', Date.today)
   end
 end
