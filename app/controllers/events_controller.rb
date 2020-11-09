a0214f6d92a216d09fdc4aaa5ff6119bcf3a3292
Damilola Oduronbi <doduronbi@gmail.com>
2020-11-09 13:52:40 +0100

    Refactor event index action to have past and upcoming events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,8 @@
 class EventsController < ApplicationController
-  def index; end
+  def index
+    @past_events = Event.past
+    @upcoming_events = Event.upcoming
+  end
 
   def show
     @event = Event.find(params[:id])
