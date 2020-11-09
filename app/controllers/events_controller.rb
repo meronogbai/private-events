9787e3338da15fe7fbbc0a56c4657e2b1f4ff387
Meron Ogbai <okbaymeron@gmail.com>
2020-11-09 15:22:53 +0300

    Let logged in user create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,22 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    event = Event.new(event_params)
+    if event.save
+      flash[:success] = 'You have created a new event!'
+      redirect_to event_path(event)
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:location, :description, :event_date)
+  end
 end
